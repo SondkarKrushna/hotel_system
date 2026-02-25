361dd1871716803e4606602f97d2ffde30f6d479
monika <monikacsns14@gmail.com>
2026-02-25 16:09:14 +0530

    Update hotelApi.js

--- src/store/Api/hotelApi.js
+++ src/store/Api/hotelApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const hotelApi = createApi({
   reducerPath: "hotelApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BACKEND_URL || "https://hotelmanagement-vcsy.onrender.com",
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.token;
